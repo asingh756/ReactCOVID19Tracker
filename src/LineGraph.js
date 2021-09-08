8fc0b5eccd2982f16ec02b92403a755367734f97
Amrit Singh <amritsingh@MacBook-Pro.local>
2021-09-07 20:13:49 -0700

    Sep 7 Commit

--- src/LineGraph.js
+++ src/LineGraph.js
@@ -2,51 +2,6 @@ import React, {useState, useEffect} from 'react'
 import { Line } from "react-chartjs-2";
 import numeral from "numeral";
 
-/*function LineGraph() {
-
-    const [data, setData] = useState({});
-
-    useEffect(() => {
-        const fetchData = async () => {
-            await fetch('https://disease.sh/v3/covid-19/historical/all?lastdays=120')
-            .then(response => response.json())
-            .then(data => {
-                const chartData = buildChartData(data);
-                setData(chartData);
-            });
-        }
-
-        fetchData();
-        
-    }, []);
-
-    const buildChartData = (data, casesType='cases') => {
-        const chartData = [];
-        let lastDataPoint;
-
-        data[casesType].forEach(date => {
-            if (lastDataPoint) {
-                const newDataPoint = {
-                    x: date, 
-                    y: data[casesType][date] - lastDataPoint
-                }
-                chartData.push(newDataPoint);
-            }
-            lastDataPoint = data[casesType][date];
-        })
-        return chartData;
-    }
-    return (
-        <div>
-            <h1>Im a graph</h1>
-            
-        </div>
-    )
-}
-
-export default LineGraph
-*/
-
 const options = {
     legend: {
       display: false,
